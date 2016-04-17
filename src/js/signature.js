d3af220275dcda415cbacb72e945dcbd49188f13
EezoME <blackup5@gmail.com>
2016-04-17 21:40:58 +0300

    Base source push

---
+++ src/js/signature.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Eezo on 17.04.2016.
+ */
+document.write("<br><br><br><p align=center >Версия 1.01 от 13.04.2015г.<br><br>Дмитрий Чуриков &#169 2015-" + new Date().getFullYear() + "</p>");
