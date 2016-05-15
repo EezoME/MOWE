b67cb27991cca870515a60b04e84fcde835744e4
EezoME <blackup5@gmail.com>
2016-05-15 19:03:12 +0300

    Large number of little fixes

--- src/js/signature.js
+++ src/js/signature.js
@@ -1,4 +1,14 @@
 /**
  * Created by Eezo on 17.04.2016.
  */
-document.write("<br><br><br><p align=center >Версия 1.01 от 13.04.2015г.<br><br>Дмитрий Чуриков &#169 2015-" + new Date().getFullYear() + "</p>");
+
+//document.write("<script src=\"http://vk.com/js/api/share.js?93\" charset=\"windows-1251\"></script>");
+var imageSize = 24;
+
+document.write("<br/><br/><br/><p align=center >Версия 1.01 от 14.05.2016<br/>");
+
+document.write("<p align='center'>Мы <a href='https://vk.com/club18039326'><img src='http://vk.com/images/share_32_eng.png' width='"+imageSize+"' height='"+imageSize+"' alt='ВКонтакте' /></a><br/>"); // use 'share_32.png' for Russian VK
+
+document.write("Дмитрий Чуриков <a href='https://vk.com/17051544'><img src='http://vk.com/images/share_32_eng.png' width='"+imageSize+"' height='"+imageSize+"' alt='ВКонтакте' /></a> <a href='https://www.facebook.com/profile.php?id=100003380499872'><img src='/images/facebook-icon.png' width='"+imageSize+"' height='"+imageSize+"' alt='Facebook' /></a></p>");
+
+document.write("<p align='center'>2010-" + new Date().getFullYear() + "</p>");
