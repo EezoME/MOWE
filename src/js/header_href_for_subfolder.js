083c3dee401d864e92beb67b7ac5db96500e3203
EezoME <blackup5@gmail.com>
2016-05-18 10:38:37 +0300

    ADD: mod creation manual

---
+++ src/js/header_href_for_subfolder.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Eezo on 18.05.2016.
+ */
+document.write("<a href='../../editor.html'><img src='../../images/home.jpg' class='homepage_ico' title=\"Перейти к главной странице\"/></a>");
